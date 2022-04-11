a293eac462875830daf0740b8c1e18f1a59c3333
EricMansilla <eric.324.st@gmail.com>
2022-04-10 23:10:05 -0300

    API Foundation

---
+++ dev/api.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+app.get('/blockchain', function (req, res) {
+    // Code
+})
+
+app.post('/transaction', function (req, res) {
+    // Code
+})
+
+app.get('/mine', function (req, res) {
+    // Code
+})
+
+app.listen(3000, function() {
+    console.log('Listening on port 3000...');
+})
